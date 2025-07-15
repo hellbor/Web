8c4b622888910e19e05fa682661caa8de1fea61e
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2025-07-15 19:04:03 +0300

    SwitchBackground DONE

--- 02-JavaScript/02-functions/functions.js
+++ 02-JavaScript/02-functions/functions.js
@@ -14,17 +14,18 @@ function SwitchBackground()
 {
     let switchButton = document.getElementById('switchBackground');
     console.log(switchButton.attributes.src);
+
     //switchButton.attributes.src.nodeValue = switchButton.attributes.src.nodeValue == 'img/moon.png' ? 'img/sun.png' : 'img/moon.png';
     if (switchButton.attributes.src.nodeValue == 'img/moon.png')
     {
-        switchButton.attributes.src.nodeValue == 'img/sun.png';
-        document.body.style.backgroundColor = "#323232";
+        switchButton.attributes.src.nodeValue = 'img/sun.png';
+        document.body.style.background = "#323232";
         document.body.style.color = "white";
     }
     else
     {
-        switchButton.attributes.src.nodeValue == 'img/moon.png';
-        document.body.style.backgroundColor = "#FFFFFF";
+        switchButton.attributes.src.nodeValue = 'img/moon.png';
+        document.body.style.background = "#FFFFFF";
         document.body.style.color = "black";
     }
 
