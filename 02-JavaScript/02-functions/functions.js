7494da5ec7a37a6b5c61ec3fb9c91acb1aeded9f
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2025-07-24 20:52:38 +0300

    100 years problem

--- 02-JavaScript/02-functions/functions.js
+++ 02-JavaScript/02-functions/functions.js
@@ -147,7 +147,27 @@ function tickCountdown()
     const SECONDS_IN_MONTH = SECONDS_IN_DAY * DAYS_IN_MONTH;
     const SECONDS_IN_YEAR = SECONDS_IN_DAY * 365 + SECONDS_IN_HOUR * 6;
 
+    ////////////////////////////////////////////////////////////////////////////////////
     let time_of_day = timestamp % SECONDS_IN_DAY;
+    //Убираем время дня из timestamp:
+    let date = Math.floor(timestamp / SECONDS_IN_DAY);
+    date = date * SECONDS_IN_DAY;
+
+    let str_date = '';
+    let years = Math.floor(date / SECONDS_IN_YEAR); str_date += `Years:${years},`;
+    date = date - years * SECONDS_IN_YEAR;
+    //if (years > 0) date = (date % (years * SECONDS_IN_YEAR));
+    let months = Math.floor(date / SECONDS_IN_MONTH); str_date += `Months:${months},`;
+    date = date - months * SECONDS_IN_MONTH;
+    //if (months > 0) date = (date % (months * SECONDS_IN_MONTH));
+    let weeks   = Math.floor(date / SECONDS_IN_WEEK);  str_date += `Weeks:${weeks},`;
+    date = date - weeks * SECONDS_IN_WEEK;
+    if (weeks > 0) date = (date % (weeks * SECONDS_IN_WEEK));
+    let days = Math.ceil(date / SECONDS_IN_DAY); str_date += `Days:${days},`;
+    document.getElementById("date-reminded").innerHTML = str_date;
+
+    ////////////////////////////////////////////////////////////////////////////////////
+
     let hours = Math.floor(time_of_day / SECONDS_IN_HOUR);
     if (hours > 0) time_of_day = (time_of_day % (hours * SECONDS_IN_HOUR));
 
@@ -155,6 +175,8 @@ function tickCountdown()
     if (minutes > 0) time_of_day = (time_of_day % (minutes * SECONDS_IN_MINUTE));
 
     let seconds = time_of_day;
+
+
     ////////////////////////////////////////////////////////////////////////////////////
 
     document.getElementById("hours-unit").innerHTML = addLeadingZero(hours);
